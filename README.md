12781297c6f67e1785d172292a5b45addc47ece8
rohansx <hello@rohan.sh>
2024-08-23 18:00:18 +0530

    migration to turbopack-nextjs

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-
 # Xmail
 
 ## Overview
--- src/types/express/index.d.ts
+++
@@ -1,6 +0,0 @@
-// src/types/express/index.d.ts
-declare namespace Express {
-  export interface Request {
-    user?: string; // Adjust the type according to your usage
-  }
-}
